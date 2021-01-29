715f83da3ce841adbaffdfa1446683675babafe4
JoeYashinsky <joey.yashinsky@gmail.com>
2021-01-29 16:14:25 -0500

    Edited OnGetAsync method within RegisterModel class to grab any role(s) in the database and initialized Roles property with value(s). Also edited OnPostAsync method so a new user role relationship is added to the database when a new user is successfully created

--- ReMenu/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ReMenu/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -81,12 +81,18 @@ namespace ReMenu.Areas.Identity.Pages.Account
         {
             returnUrl = returnUrl ?? Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            var roles = _roleManager.Roles;
+            Roles = new SelectList(roles, "Name");
             if (ModelState.IsValid)
             {
                 var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    if(await _roleManager.RoleExistsAsync(Input.Role))
+                    {
+                        await _userManager.AddToRoleAsync(user, Input.Role);
+                    }
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
