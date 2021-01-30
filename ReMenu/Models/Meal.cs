f06442dd72367ee78389f11f29b512594b1522fb
JoeYashinsky <joey.yashinsky@gmail.com>
2021-01-30 17:26:25 -0500

    Created new Model class called 'Meal'

---
+++ ReMenu/Models/Meal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReMenu.Models
+{
+    public class Meal
+    {
+        [Key]
+        public int MealId { get; set; }
+
+        [ForeignKey("Foodie")]
+        public int FoodieId { get; set; }
+        public Foodie Foodie { get; set; }
+    }
+}
