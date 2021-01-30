850c4039aa83b91d398b7feb2e5407175fbf9932
JoeYashinsky <joey.yashinsky@gmail.com>
2021-01-30 12:18:02 -0500

    GlobalRouting class created in new ActionFilters folder, with the class inheriting from IActionFilter

--- ReMenu/Startup.cs
+++ ReMenu/Startup.cs
@@ -12,6 +12,9 @@ using ReMenu.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using ReMenu.ActionFilters;
 
 namespace ReMenu
 {
@@ -35,6 +38,13 @@ namespace ReMenu
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultUI()
                 .AddDefaultTokenProviders();
+
+            services.AddScoped<ClaimsPrincipal>(s => 
+                s.GetService<IHttpContextAccessor>().HttpContext.User);
+            services.AddControllers(config =>
+            {
+                config.Filters.Add(typeof(GlobalRouting));
+            });
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
