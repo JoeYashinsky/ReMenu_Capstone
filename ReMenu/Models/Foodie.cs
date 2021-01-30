7a10feb08b78d4358a795d7305518de513b6d391
JoeYashinsky <joey.yashinsky@gmail.com>
2021-01-30 11:56:47 -0500

    Added a 'Foodie' model to represent a user of the ReMenu app

---
+++ ReMenu/Models/Foodie.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReMenu.Models
+{
+    public class Foodie
+    {
+        [Key]
+        public int FoodieId { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
