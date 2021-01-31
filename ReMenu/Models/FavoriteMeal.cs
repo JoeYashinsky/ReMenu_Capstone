2c72d4404d42c5d06409caedd28ed41331970364
JoeYashinsky <joey.yashinsky@gmail.com>
2021-01-31 14:57:38 -0500

    Added properties to Restaurant model class and created a new model class called 'FavoriteMeal'

---
+++ ReMenu/Models/FavoriteMeal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReMenu.Models
+{
+    public class FavoriteMeal
+    {
+        [Key]
+        public int FavoriteMealId { get; set; }
+
+        [ForeignKey("Foodie")]
+        public int FoodieId { get; set; }
+        public Foodie Foodie { get; set; }
+
+        [ForeignKey("Restaurant")]
+        public int RestaurantId { get; set; }
+        public Restaurant Restaurant { get; set; }
+
+        [ForeignKey("Meal")]
+        public int MealId { get; set; }
+        public Meal Meal { get; set; }
+
+    }
+}
