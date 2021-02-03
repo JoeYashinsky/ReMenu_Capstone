4e7343c587bdc1fe90c757aa2ec5c4aa03c5fbdb
JoeYashinsky <joey.yashinsky@gmail.com>
2021-02-03 14:55:00 -0500

    FoodiesController added and Index action logic added

---
+++ ReMenu/Controllers/FoodiesController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReMenu.Interfaces;
+using ReMenu.Models;
+
+namespace ReMenu.Controllers
+{
+    public class FoodiesController : Controller
+    {
+        private IInterfaceWrapper _repo;
+
+        public FoodiesController(IInterfaceWrapper repo)
+        {
+            _repo = repo;
+        }
+
+        // GET: FoodiesController
+        public async Task<ActionResult> Index()
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            Foodie foodie = await _repo.Foodie.GetFoodieAsync(userId);
+            if (foodie == null)
+            {
+                return RedirectToAction("Create");
+            }
+
+            return View();
+        }
+
+        // GET: FoodiesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: FoodiesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FoodiesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FoodiesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FoodiesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FoodiesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FoodiesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
