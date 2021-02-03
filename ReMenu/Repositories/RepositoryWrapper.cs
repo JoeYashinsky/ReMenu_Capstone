d5ce3f87e7737407f0e9767fee8237d293cf85bc
JoeYashinsky <joey.yashinsky@gmail.com>
2021-02-03 03:36:44 -0500

    Added two classes to repositories folder, with RestaurantRepository implements the IRestaurantInterface

---
+++ ReMenu/Repositories/RepositoryWrapper.cs
@@ -0,0 +1,56 @@
+﻿using ReMenu.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReMenu.Repositories
+{
+    public class RepositoryWrapper : IInterfaceWrapper
+    {
+        private ApplicationDbContext _context;
+        private IFoodieInterface _foodie;
+        private IMealInterface _meal;
+        private IRestaurantInterface restaurant;
+
+        public IFoodieInterface Foodie
+        {
+            get
+            {
+                if(_foodie == null)
+                {
+                    _foodie = new FoodieRepository(_context);
+                }
+                return _foodie;
+            }
+        }
+
+
+
+        public IMealInterface Meal
+        {
+            get
+            {
+                if(_meal == null)
+                {
+                    _meal = new MealRepository(_context);
+                }
+                return _meal;
+            }
+        }
+
+
+
+        public IRestaurantInterface Restaurant
+        {
+            get
+            {
+                if(_restaurant == null)
+                {
+                    _restaurant = new RestaurantRepository(_context);
+                }
+                return _restaurant;
+            }
+        }
+    }
+}
---
+++ ReMenu/Repositories/RestaurantRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReMenu.Data;
+using ReMenu.Interfaces;
+using ReMenu.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReMenu.Repositories
+{
+    public class RestaurantRepository : BaseRepository<Restaurant>, IRestaurantInterface
+    {
+        private readonly object repo;
+
+        public RestaurantRepository(ApplicationDbContext applicationDb) : base(applicationDb)
+        {
+
+        }
+
+        public void CreateRestaurant(Restaurant restaurant) => Create(restaurant);
+        public void EditRestaurant(Restaurant restaurant) => Update(restaurant);
+        public void DeleteRestaurant(Restaurant restaurant) => Delete(restaurant);
+        public async Task<List<Restaurant>> GetRestaurantsAsync() => await FindAll().ToListAsync();
+        public async Task<Restaurant> GetRestaurantAsync(int restaurantId) => await FindByCondition(r => r.RestaurantId.Equals(restaurantId)).FirstOrDefaultAsync();
+    }
+}
